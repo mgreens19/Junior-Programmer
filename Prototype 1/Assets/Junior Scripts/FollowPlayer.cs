52a162b796bd9b885aaf02ffbc50186c1daee2c1
Maddox <mgreenspoon19@gmail.com>
2025-11-06 08:43:48 -0500

    11/6/2025

--- Prototype 1/Assets/Junior Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Junior Scripts/FollowPlayer.cs
@@ -4,7 +4,6 @@ public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
     private Vector3 offset = new Vector3(0, 6, -15);
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -18,3 +17,4 @@ public class FollowPlayer : MonoBehaviour
         transform.position = player.transform.position + offset;
     }
 }
+
--- Prototype 1/Assets/Junior Scripts/PlayerController.cs
+++ Prototype 1/Assets/Junior Scripts/PlayerController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float slideSpeed;
+    public float horizontalInput;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,5 +18,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the Vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * slideSpeed);
     }
 }
