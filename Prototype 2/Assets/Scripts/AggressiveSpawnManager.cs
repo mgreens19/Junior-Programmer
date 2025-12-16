8a0555e54894aff030796b72d554e7fc767a3986
Maddox <mgreenspoon19@gmail.com>
2025-12-16 08:45:04 -0500

    12/16/2025

--- Prototype 2/Assets/Scripts/AggressiveSpawnManager.cs
+++ Prototype 2/Assets/Scripts/AggressiveSpawnManager.cs
@@ -5,9 +5,10 @@ public class AggressiveSpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeZUpper = 16.0f;
     private float spawnRangeZLower = 1.0f;
-    private float spawnPosX = 20;
+    private float spawnPosX;
     private float startDelay = 2;
     private float spawnInterval = 0.5f;
+    private Quaternion chosenRotation;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,23 +20,39 @@ public class AggressiveSpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+       
     }
 
-    void SpawnRandomAnimal(float spawnX, bool faceRight)
+    void SpawnRandomAnimal()
     {
-        // Returns random int between 0-1
-        int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-        Quaternion rotation;
-        if (faceRight)
-            rotation = Quaternion.Euler(0, 90, 0);
-        else
-            rotation = Quaternion.Euler(0, -90, 0);
+        Quaternion rotationRight = Quaternion.Euler(0, 90, 0);
 
-            // Returns a random z position between -20 and 20 at a x position of 20
-            Vector3 spawnPos = new Vector3(spawnPosX, 0, Random.Range(-spawnRangeZLower, spawnRangeZUpper));
+        Quaternion rotationLeft = Quaternion.Euler(0, -90, 0);
+
+       
+
+        if (Random.value < 0.5f)
+        {
+            spawnPosX = -20f;
+            chosenRotation = rotationRight;
+        }
+        else 
+        {
+            spawnPosX = 20f;
+            chosenRotation = rotationLeft;
+        }
+
+
+            // Returns random int between 0-1
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        // Returns a random z position between -20 and 20 at a x position of 20
+        Vector3 spawnPos = new Vector3(spawnPosX, 0, Random.Range(-spawnRangeZLower, spawnRangeZUpper));
+
+
+
         // Creates an animal with the corresponding array int that is returned at a random position
-        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, chosenRotation);
     }
 }
